b2dc9346a7717ca2e26e93343cee1bb8c9f1560a
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-05-05 21:42:20 +0100

    add sql file to create tables

---
+++ db/moneydboardtables.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS loans;
+DROP TABLE IF EXISTS incomes;
+
+
+DROP TABLE IF EXISTS transfers;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS mastercategories;
+
+CREATE TABLE users(
+  id SERIAL4 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  dob DATE,
+  over_budget BOOL
+);
+
+CREATE TABLE mastercategories(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE categories(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  master_category_id INT4 REFERENCES mastercategories(id)
+);
+
+CREATE TABLE transfers(
+  id SERIAL4 PRIMARY KEY,
+  amount NUMERIC(8,2),
+  op_date DATE,
+  time time,
+  user_id INT4 REFERENCES users(id),
+  category_id INT4 REFERENCES categories(id) ON DELETE CASCADE
+);
+
+CREATE TABLE incomes(
+  id SERIAL4 PRIMARY KEY,
+  amount NUMERIC(8,2),
+  user_id INT4 REFERENCES users(id)
+);
+
+CREATE TABLE loans(
+  id SERIAL4 PRIMARY KEY,
+  amount NUMERIC(8,2),
+  date DATE,
+  provider VARCHAR(255),
+  user_id INT4 REFERENCES users(id)
+);
