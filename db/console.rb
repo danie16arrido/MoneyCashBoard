bf9c7d89e8c3d7f34d9216b21e661b88c600aadd
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-05-06 15:26:46 +0100

    add CRUD methods for Category class

--- db/console.rb
+++ db/console.rb
@@ -1,6 +1,7 @@
 require('pry-byebug')
 
 require_relative('../models/mastercategory.rb')
+require_relative('../models/category.rb')
 
 
 
--- models/category.rb
+++ models/category.rb
@@ -7,4 +7,47 @@ class Category
     @master_category_id = params['master_category_id'].to_i
   end
 
+  def save()
+    sql = "
+    INSERT INTO categories
+    (name, master_category_id) VALUES ('#{@name}', '#{@master_category_id}')
+    RETURNING id;
+    "
+    category_data = SqlRunner.run(sql)
+    @id = category_data.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "
+    SELECT * FROM categories;
+    "
+    result = SqlRunner.run(sql)
+    categories_hash = result.map { |a_category| Category.new(a_category) }
+    return categories_hash
+  end
+
+  def self.find_by_id(id)
+    sql = "
+    SELECT * FROM categories WHERE id = #{id};
+    "
+    result = SqlRunner.run(sql)
+    return Category.new(result.first())
+  end
+
+  def update()
+    sql = "
+    UPDATE categories SET
+    (name, master_category_id) = ('#{@name}', '#{@master_category_id}')
+    WHERE id = #{@id};
+    "
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "
+    DELETE FROM categories WHERE id = #{@id};
+    "
+    SqlRunner.run(sql)
+  end
+
 end
