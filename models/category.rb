ff7fec4d5c679cba76f514108be7351d42cc60bf
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-05-06 12:57:00 +0100

    add Category class and TDD it

---
+++ models/category.rb
@@ -0,0 +1,9 @@
+class Category
+  attr_reader :id
+  attr_accessor :name, :master_category_id
+  def initialize(params)
+    @id = params['id'] if params['id']
+    @name = params['name']
+    @master_category_id = params['master_category_id']
+  end
+end
---
+++ specs/category_spec.rb
@@ -0,0 +1,28 @@
+require('minitest/autorun')
+require('minitest/emoji')
+require_relative('../models/category.rb')
+
+
+class TestCategory < MiniTest::Test
+  def setup()
+    category1_data = {
+      'id' => 1,
+      'name' => "Gas",
+      'master_category_id' => 1
+    }
+    @category1 = Category.new(category1_data)
+  end
+
+  def test_can_create_class()
+    assert_equal(Category, @category1.class)
+  end
+
+  def test_can_update_name()
+    @category1.name = "Electricity"
+    assert_equal("Electricity", @category1.name)
+  end
+
+  def test_can_get_int_id()
+    assert_equal(1, @category1.id)
+  end
+end
