fca8f9a3d3253aad3b3043063ccc83995373d2b7
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-05-06 15:36:38 +0100

    add delete_all method to Mastercategory and Category

--- db/console.rb
+++ db/console.rb
@@ -3,11 +3,13 @@ require('pry-byebug')
 require_relative('../models/mastercategory.rb')
 require_relative('../models/category.rb')
 
-MasterCategory.delete_all
+MasterCategory.delete_all()
+Category.delete_all()
 
 mcat = MasterCategory.new({'name' => "Bills"})
 mcat.save()
 cate = Category.new({'name' => "Gas", 'master_category_id' => mcat.id})
+cate.save()
 
 
 
